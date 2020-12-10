3650cbab48af78ae5bfe0b8fa2b93b885a1b08df
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:13:05 +0200

    Create Petunia

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class Petunia : Magnoliophyta
+    {
+        public Petunia(string medicinal, string zone)
+           : base(medicinal, zone)
+        {
+
+        }
+        public override string Domain => "Eukaryota";
+
+        public override TypeOfFamilia TypeOfFamilia => TypeOfFamilia.Solanaceae;
+
+        public override string Name => "Petunia";
+
+        public override TimeSpan LifeTime => TimeSpan.FromDays(365);
+
+        public override string NameKingdom => "Plantae";
+    }
+}
