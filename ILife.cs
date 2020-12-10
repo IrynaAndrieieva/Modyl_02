4d16cfca471cdd5fe16bb241b01ea176697412d6
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:07:35 +0200

    Added interfase ILife

---
+++ ILife.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public interface ILife
+    {
+        string Domain { get; }
+
+        string Name { get; }
+
+        string Medicinal { get; }
+
+        TimeSpan LifeTime { get; }
+
+        string Zone { get; }
+
+    }
+}
