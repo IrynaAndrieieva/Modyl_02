77f2625dc6b6b4289aba3f5012f3b2a0f7eb44b5
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:21:12 +0200

    Added FencingServis

---
+++ FencingServis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    class FencingServis : IFencingServis
+    {
+        public FencingServis()
+        {
+        }
+        public Fencing MakeMedicinPlant()
+        {
+            var magnoliophyta = new Magnoliophyta[]
+            {
+                new Mentha("Yes", "Forest zone"),
+                new ValerianaOfficinalis("Yes", "Forest zone"),
+            };
+           
+            return new Fencing { Magnoliophyta = magnoliophyta };
+        }
+
+        public Fencing MakeNonMedicinPlant()
+        {
+            var magnoliophyta = new Magnoliophyta[]
+            {
+                new Petunia("No", "Equatorial and subequatorial"),
+            };
+
+            return new Fencing { Magnoliophyta = magnoliophyta };
+        }
+    }
+}
