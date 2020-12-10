3d383723a76a6a75b794edcc8f288e5be7bcf4bb
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:16:36 +0200

    Create Comparer from Magnoliophy

---
+++ MagnoliophytaComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class MagnoliophytaComparer : IComparer
+    {
+
+        public int Compare(object first, object second)
+        {
+            var x = first as Magnoliophyta;
+            var y = second as Magnoliophyta;
+
+            if (x.Name.Length < y.Name.Length)
+            {
+                return 1;
+            }
+            else if (x.Name.Length > y.Name.Length)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Mentha.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class Mentha : Magnoliophyta
+    {
+        public Mentha(string medicinal, string zone)
+           : base(medicinal, zone)
+        {
+
+        }
+        public override string Domain => "Eukaryota";
+
+        public override TypeOfFamilia TypeOfFamilia => TypeOfFamilia.Lamiaceae;
+
+        public override string Name => "Mentha";
+
+        public override TimeSpan LifeTime => TimeSpan.FromDays(365);
+
+        public override string NameKingdom => "Plantae";
+    }
+}
