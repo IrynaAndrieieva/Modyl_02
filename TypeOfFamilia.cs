a4d5b21e77a1d6048922e659bb6fcc2731239799
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:11:07 +0200

    Added different  enum Familia

---
+++ TypeOfFamilia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public enum TypeOfFamilia //Семейство
+    {
+        Lamiaceae, //семейство Яснотковые
+        Caprifoliaceae, // семейство Жимолостные
+        Solanaceae, // семейство Пасленовые
+    }
+}
