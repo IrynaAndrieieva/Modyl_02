d21144f386b4997eb7503dfd0a527cadec2a55f0
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:10:01 +0200

    Added abstract class Magnoliophyta

---
+++ Magnoliophyta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public abstract class Magnoliophyta : Kingdom //Отдел Цветковые
+    {
+        protected Magnoliophyta(string medicinal, string zone)
+            : base(medicinal, zone)
+        {
+
+        }
+
+        public abstract TypeOfFamilia TypeOfFamilia { get; }
+
+        //public abstract Ingredient[] HerbalPreparation { get; }
+    }
+}
