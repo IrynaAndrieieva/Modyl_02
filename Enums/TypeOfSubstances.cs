57aafb995efc0d27c59e8fc799d87e4d9112077c
Iryna <ortofosfatnyi@gmail.com>
2020-12-11 00:34:05 +0200

    Create Recipe fron plants

---
+++ Enums/TypeOfSubstances.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02.Enums
+{
+    public enum TypeOfSubstances
+    {       
+        Sacchari, //сахар
+        Aqua, // вода
+        Capsularis, // капсула
+        Lagena, // бутылка
+
+    }
+}
---
+++ Models/Substances.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modyl_02.Enums;
+using System.Threading.Tasks;
+
+namespace Modyl_02.Models
+{ 
+    public abstract class Substances : Magnoliophyta //Отдел Цветковые
+    {
+        protected Substances(string medicinal, string zone)
+            : base(medicinal, zone)
+        {
+        }
+
+        public abstract TypeOfSubstances TypeOfSubstances { get; }      
+    }
+}
