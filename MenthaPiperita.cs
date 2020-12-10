260cedd5b61196cc40af96dd15938f13672d48de
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:14:15 +0200

    Create MenthaPiperita and rename Class1

---
+++ MenthaPiperita.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class MenthaPiperita : Magnoliophyta
+    {
+        protected MenthaPiperita(string medicinal, string zone)
+           : base(medicinal, zone)
+        {
+
+        }
+        public override string Domain => "Eukaryota";
+
+        public override TypeOfFamilia TypeOfFamilia => TypeOfFamilia.Lamiaceae;
+
+        public override string Name => "MenthaPiperita";
+
+        public override TimeSpan LifeTime => TimeSpan.FromDays(365);
+
+        public override string NameKingdom => "Plantae";
+    }
+}
