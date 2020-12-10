a07946e395867b655ff2ae88a0563e0e2c59172d
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:17:26 +0200

    Make Fencing from Plants

---
+++ Fencing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class Fencing
+    {
+        public Magnoliophyta[] Magnoliophyta { get; set; }
+    }
+}
