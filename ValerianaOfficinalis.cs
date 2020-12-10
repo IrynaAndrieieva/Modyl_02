27b338980122ba847278c17df0359b762644e0e8
Iryna <ortofosfatnyi@gmail.com>
2020-12-10 23:12:28 +0200

    Added Valeriana Officinalis

---
+++ ValerianaOfficinalis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_02
+{
+    public class ValerianaOfficinalis : Magnoliophyta
+    {
+        public ValerianaOfficinalis(string medicinal, string zone)
+           : base(medicinal, zone)
+        {
+
+        }
+        public override string Domain => "Eukaryota";
+
+        public override TypeOfFamilia TypeOfFamilia => TypeOfFamilia.Caprifoliaceae;
+
+        public override string Name => "ValerianaOfficinalis";
+
+        public override TimeSpan LifeTime => TimeSpan.FromDays(365);
+
+        public override string NameKingdom => "Plantae";
+    }
+}
